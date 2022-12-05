f85314c6f9964c7485b5e45d3db722c05e6dde16
ModinaEN <modinaen@gmail.com>
2022-12-05 21:58:04 +0300

    Расчитывает расстояние между двумя точками в пространстве

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
-﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-Console.Write($"Введите пятизначное число \n");
-//int a = int.Parse(Console.ReadLine()!);
-String number = Console.ReadLine()!;
-if (number[4] == number[0] & number[3] == number[1]) 
-{Console.Write($"число {number} является палиндромом \n");}
-else {Console.Write($"число {number} не является палиндромом \n");}
+int [] tochkaA = new int[3];
+int [] tochkaB = new int[3];
+double [] d = new double [3];
+double dist = 0;
+int n1 = 0;
+while (n1<3)
+{Console.Write($"Введите координату точки А x{n1+1}=");
+tochkaA[n1] = int.Parse(Console.ReadLine());
+Console.Write($"Введите координату точки B x{n1+1}=");
+tochkaB[n1] = int.Parse(Console.ReadLine());
+d[n1] = Math.Pow((tochkaA[n1]-tochkaB[n1]),2);
+dist = dist + d[n1];
+n1++;}
+//d[n1] = Math.Pow((tochkaA[n1]-tochkaB[n1]),2);
+//d2 = Math.Pow((tochkaA[1]-tochkaB[1]),2);
+//d3 = Math.Pow((tochkaA[2]-tochkaB[2]),2);
+dist = Math.Round(Math.Sqrt(dist),2);
+Console.Write($"Расстояние между точкой А и точкой В равно {dist}");
