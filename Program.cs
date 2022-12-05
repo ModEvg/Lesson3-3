d1f1e08480fdda16ecc857f1568422eee39211f7
ModinaEN <modinaen@gmail.com>
2022-12-05 22:26:06 +0300

    Создана программа для выведения таблицы кубов чисел от 1 до N

--- Program.cs
+++ Program.cs
@@ -1,20 +1,12 @@
-﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-int [] tochkaA = new int[3];
-int [] tochkaB = new int[3];
-double [] d = new double [3];
-double dist = 0;
-int n1 = 0;
-while (n1<3)
-{Console.Write($"Введите координату точки А x{n1+1}=");
-tochkaA[n1] = int.Parse(Console.ReadLine());
-Console.Write($"Введите координату точки B x{n1+1}=");
-tochkaB[n1] = int.Parse(Console.ReadLine());
-d[n1] = Math.Pow((tochkaA[n1]-tochkaB[n1]),2);
-dist = dist + d[n1];
-n1++;}
-//d[n1] = Math.Pow((tochkaA[n1]-tochkaB[n1]),2);
-//d2 = Math.Pow((tochkaA[1]-tochkaB[1]),2);
-//d3 = Math.Pow((tochkaA[2]-tochkaB[2]),2);
-dist = Math.Round(Math.Sqrt(dist),2);
-Console.Write($"Расстояние между точкой А и точкой В равно {dist}");
+Console.Write($"Введите число N ");
+int N = int.Parse(Console.ReadLine()!);
+double [] array = new double[N];
+int index = 0;
+while (index<N)
+{
+array[index] = Math.Pow(index+1,3);
+Console.Write($" {array[index]} ");
+index++;
+}
